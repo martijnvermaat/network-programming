87631456dea45e4c2e41389a5d6f87efeb1c4943
Martijn Vermaat <martijn@vermaat.name>
2007-02-19 13:48:14 +0000

    Ook mysh1 opgeruimd.

--- mysh1.c
+++ mysh1.c
@@ -1,4 +1,4 @@
-
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
@@ -8,7 +8,6 @@
 #include <stdlib.h>
 #include <errno.h>
 
-#define MAX_INPUT_LENGTH 100
 #define DEBUG 1
 
 #ifdef DEBUG
@@ -17,35 +16,45 @@
 #define dprint (void)
 #endif
 
+int exec_command(char *line) {
+  char *name;
+  name = strrchr(line, '/');
+  if(name == NULL) {
+    name = line;
+  } else {
+    name = name+1; // skip slash
+  }
+  execlp(line, name, NULL);
+  perror(line); // only reached if execlp fails
+  return 1;
+}
 
 int main(int argc, char **argv) {
 	pid_t pid;
-    char input[MAX_INPUT_LENGTH];
-    char *name;
+    char *input = NULL;
+    int input_size;
     char *exitshell = "exit";
     int status;    
 
-    while(fgets(input, MAX_INPUT_LENGTH, stdin) != NULL) {
+    while(getline(&input, &input_size, stdin) != -1) {
         input[strlen(input)-1] = 0; // overwrite newline
         if(!strcmp(input, exitshell)) { // equal to exit
-            break;
+          break;
         }
         pid = fork();
+        if (pid != 0) {
+          free(input);
+          input = NULL;
+        }
         if(pid <0) { perror("Fork error"); exit(1); }
         if(pid==0) { // child process
-            name = strrchr(input, '/');
-            if(name == NULL) {
-                name = input;
-            } else {
-                name = name+1; // skip slash
-            }
-            execlp(input, name, NULL);
-            perror(input); // only reached if execlp fails
-            exit(1);
+          exec_command(input);
+          //free(input);
+          exit(1);
         } else { // parent process
-            wait(&status); // wait for child to exit
+          wait(&status); // wait for child to exit
         }
     }
-    
+
     exit(0);
 }
