1a17538e11d2e0585b0da2c604e8416c885c123e
Martijn Vermaat <martijn@vermaat.name>
2007-05-01 11:20:25 +0000

    Weer wat verder!

---
+++ assignment-4/site-vu-mvermaat.php
@@ -0,0 +1,6 @@
+<?
+$BASEPHP            = 'http://www.cs.vu.nl/~mvermaat/np/';
+$BASECGI            = 'http://www.cs.vu.nl/~mvermaat/np-cgi/';
+$HOTEL_GATEWAY_ADDR = 'localhost';
+$HOTEL_GATEWAY_PORT = 3242;
+?>
