34b9c3948ef52e160afddbc7de59a98588902de3
Martijn Vermaat <martijn@vermaat.name>
2007-05-02 09:45:12 +0000

    Christina configs erbij.

---
+++ assignment-4/site-christina-martijn.php
@@ -0,0 +1,6 @@
+<?
+$BASEPHP            = 'http://sommelsdijk.xs4all.nl/np-martijn/';
+$BASECGI            = 'http://sommelsdijk.xs4all.nl/np-thomas/';
+$HOTEL_GATEWAY_ADDR = 'localhost';
+$HOTEL_GATEWAY_PORT = 3242;
+?>
